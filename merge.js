c8f783939d462566b1ec1edfff49ecac7dc290fd
twmnp <thunwawa48@gmail.com>
2025-01-24 10:03:42 +0700

    commit

--- merge.js
+++ merge.js
@@ -4,28 +4,22 @@ exports.merge = merge;
 function merge(collection_1, collection_2, collection_3) {
     var result = [];
     var i = 0, j = 0, k = collection_3.length - 1;
-    while (i < collection_1.length && j < collection_2.length && k >= 0) {
-        if (collection_1[i] <= collection_2[j] && collection_1[i] <= collection_3[k]) {
+    while (i < collection_1.length || j < collection_2.length || k >= 0) {
+        if (i < collection_1.length && (j >= collection_2.length || collection_1[i] <= collection_2[j]) && (k < 0 || collection_1[i] <= collection_3[k])) {
             result.push(collection_1[i++]);
         }
-        else if (collection_2[j] <= collection_1[i] && collection_2[j] <= collection_3[k]) {
+        else if (j < collection_2.length && (i >= collection_1.length || collection_2[j] <= collection_1[i]) && (k < 0 || collection_2[j] <= collection_3[k])) {
             result.push(collection_2[j++]);
         }
-        else {
+        else if (k >= 0) {
             result.push(collection_3[k--]);
         }
     }
-    while (i < collection_1.length)
-        result.push(collection_1[i++]);
-    while (j < collection_2.length)
-        result.push(collection_2[j++]);
-    while (k >= 0)
-        result.push(collection_3[k--]);
     return result;
 }
 if (require.main === module) {
     var collection_1 = [1, 3, 5];
     var collection_2 = [2, 4, 6];
     var collection_3 = [9, 8, 7];
-    console.log(merge(collection_1, collection_2, collection_3));
+    console.log(merge(collection_1, collection_2, collection_3)); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 }
